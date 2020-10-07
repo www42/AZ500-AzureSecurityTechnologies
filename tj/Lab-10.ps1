af2dd67551414148ddc905df3433faee759c43e6
www42 <thomas.jaekel@gfn.de>
2020-10-07 16:36:52 +0200

    lab10

---
+++ tj/Lab-10.ps1
@@ -0,0 +1,7 @@
+
+# sqlApp
+# -------
+#   appId:         7c9c7250-de77-4333-8078-622d2c1db509
+#   clientSecret:  kL.000II.anx.48CcMg5yqMaZ9Qz5aWp3-
+
+$kvName = (Get-AzKeyVault -ResourceGroupName 'AZ500LAB10').VaultName
