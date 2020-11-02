9c5f47fd247e6278e10443e0b4630c168f0138d9
www42 <thomas.jaekel@gfn.de>
2020-11-02 20:57:37 +0100

    Lab-01 updated

---
+++ tj/Lab-01.ps1
@@ -0,0 +1,25 @@
+# Exercise 1
+# ----------
+# Portal
+
+# Exercise 2
+# ----------
+# Task 1
+# 
+$passwordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
+$passwordProfile.Password = 'Pa55w.rd1234'
+Connect-AzureAD
+$domainName = ((Get-AzureAdTenantDetail).VerifiedDomains)[0].Name
+Get-AzureADUser
+
+# Task 2
+#
+New-AzureADGroup -DisplayName 'Junior Admins' -MailEnabled $false -SecurityEnabled $true -MailNickName JuniorAdmins
+Get-AzureADGroup
+$user = Get-AzureADUser -Filter "MailNickName eq 'Isabel'"
+Add-AzADGroupMember -MemberUserPrincipalName $user.userPrincipalName -TargetGroupDisplayName "Junior Admins" 
+Get-AzADGroupMember -GroupDisplayName "Junior Admins"
+
+
+# Exercise 3
+#  --> Lab-01.azcli
